6403cc3c64e8f13c5f740517df57e8358d5c00f1
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2024-03-23 18:57:41 +0100

    Small changes

---
+++ Ace7Ed/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ace7Ed
+{
+    internal static class Constants
+    {
+        public static char[] DatLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
+    }
+}
